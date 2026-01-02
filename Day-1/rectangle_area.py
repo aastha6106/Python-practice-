03826e540bbae2bd68e4a98a95d5eefce0545386
aastha6106 <jaibhavanipapercompany@gmail.com>
2026-01-02 19:32:17 +0530

    Add files via upload

---
+++ Day-1/rectangle_area.py
@@ -0,0 +1,4 @@
+length = int(input("Enter the length of the rectangle: "))
+breadth = int(input("Enter the breadth of the reactangle: "))
+area = length * breadth
+print("The area of the reactangle is: ", area)
