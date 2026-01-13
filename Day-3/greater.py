f3fee64c89e325451a98378067bcd146dd1ced04
aastha6106 <jaibhavanipapercompany@gmail.com>
2026-01-13 23:18:03 +0530

    Add files via upload

---
+++ Day-3/greater.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter second number: "))
+if a > b:
+    print("The first number is greater.")
+else:
+    print("The second number is greater.")
