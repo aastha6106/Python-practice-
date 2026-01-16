5baf08415b8b83a734d0491ec1c9f937b613958b
aastha6106 <jaibhavanipapercompany@gmail.com>
2026-01-16 23:02:55 +0530

    Add files via upload

---
+++ int_def.py
@@ -0,0 +1,20 @@
+age = 27
+height = 7.8
+name = "Vellore"
+is_human = True
+
+print("Integer: ")
+print("Value: ", age)
+print("Type: ",  type(age))
+
+print("Float: ")
+print("Height: ", height)
+print("Type: ", type(height))
+
+print("String: ")
+print("Name", name)
+print("Type", type(name))
+
+print("Boolean: ")
+print("Human: ", is_human)
+print("Type: ", type(is_human))
