d5dee59acdeaab0f96fa9a6fb1845fd3ce5cbafd
aastha6106 <jaibhavanipapercompany@gmail.com>
2026-01-16 22:24:05 +0530

    Add files via upload

---
+++ Day-3/traingle_type.py
@@ -0,0 +1,18 @@
+side1 = float(input("Enter the first side of the traingle: "))
+side2 = float(input("Enter the second side of the traingle: "))
+side3 = float(input("Enter the third side of the traingle: "))
+
+if side1 > 0  and side2 > 0 and side3 > 0:
+
+    if(side1 + side2 > side3) and (side2 + side3 > side1) and (side1 + side3 > side2):
+        if side1 == side2 == side3:
+            print("Equilateral Traingle.")
+        elif side1 == side2 or side2 == side3 == side1 == side3:
+            print("Isosceles Traingle")
+        else:
+            print("Scalene Traingle.")
+    else:
+        print("Invalid Traingle")
+else:
+    print("Invalid Traingle: All sides should be positive.")
+  
