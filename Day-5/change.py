055f9106edebfdd2143f911b6af7f27a44afdb26
aastha6106 <jaibhavanipapercompany@gmail.com>
2026-01-22 21:37:34 +0530

    Add files via upload

---
+++ Day-5/change.py
@@ -0,0 +1,6 @@
+my_list = [10, 20, 30, 40, 50]
+print(f"Original list: {my_list}")
+print(f"3rd element (index 2): {my_list[2]}")
+my_list[2] = 99
+print(f"Modified list: {my_list}")
+print(f"New 3rd element (index 2): {my_list[2]}")
