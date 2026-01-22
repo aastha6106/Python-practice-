f51670cee0bd167e7cc8befe83349a0dbf581858
aastha6106 <jaibhavanipapercompany@gmail.com>
2026-01-22 21:27:02 +0530

    Add files via upload

---
+++ Day-5/first_mid_last.py
@@ -0,0 +1,8 @@
+my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+first_element = my_list[0]
+middle_element = my_list[len(my_list) // 2]
+last_element = my_list[-1]
+print("First element:", first_element)
+print("Middle element:", middle_element)
+print("Last element:", last_element)
+print("Full list:", my_list)
